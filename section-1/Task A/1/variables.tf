0bfdf321f7d8b648f7bd592cca3a65748fb677ec
turansky-erik <turansky.erik@gmail.com>
2026-03-14 20:00:08 +0100

    Add files via upload

---
+++ section-1/Task A/1/variables.tf
@@ -0,0 +1,18 @@
+
+variable "var_region" {
+	type = string
+	default = "eu-central-1"
+	}
+
+
+variable "var_access_key" {
+	type = string
+	default = "XXXXXXXXXXXXXXXXXXX"
+	}
+
+
+
+variable "var_secret_key" {
+	type = string
+	default = "XXXXXXXXXXXXXXXX"
+	}
